46575c0fd3105e40eb6c6e3af98915002737de00
lywworking <644543260@qq.com>
2023-06-27 13:45:59 +0800

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Button } from "antd-mobile";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hello Antd Mobile</h1>
+      <Button>123</Button>
+    </div>
+  );
+}
